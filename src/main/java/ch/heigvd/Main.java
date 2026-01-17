69b9bde1ce8f8bc89d661c95e814f0eb2806a664
Ischim <marc.ischi@hes-so.ch>
2026-01-17 23:06:45 +0100

    heart of api

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -3,35 +3,56 @@ package ch.heigvd;
 import ch.heigvd.auth.AuthController;
 import ch.heigvd.users.User;
 import ch.heigvd.users.UsersController;
+import ch.heigvd.messages.Message;
+import ch.heigvd.messages.MessagesController;
+
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class Main {
-  public static final int PORT = 8080;
+    public static final int PORT = 443;
 
-  public static void main(String[] args) {
-    Javalin app = Javalin.create();
+    public static void main(String[] args) {
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "public";
+                staticFileConfig.location = Location.CLASSPATH;
+            });
+        });
 
-    // This will serve as our database
-    ConcurrentMap<Integer, User> users = new ConcurrentHashMap<>();
+        // This will serve as our database
+        ConcurrentMap<Integer, User> users = new ConcurrentHashMap<>();
+        ConcurrentMap<Integer, Message> messages = new ConcurrentHashMap<>();
+        ConcurrentMap<String, Integer> cookies = new ConcurrentHashMap<>();
 
-    // Controllers
-    AuthController authController = new AuthController(users);
-    UsersController usersController = new UsersController(users);
+        // Controllers
+        AuthController authController = new AuthController(users, messages,cookies);
+        UsersController usersController = new UsersController(users, cookies);
+        MessagesController messagesController = new MessagesController(messages, users, cookies);
 
-    // Auth routes
-    app.post("/login", authController::login);
-    app.post("/logout", authController::logout);
-    app.get("/profile", authController::profile);
+        // Users routes
+        app.post  ("/users",          usersController::create);
+        app.patch ("/users/{userId}", usersController::update);
+        app.get   ("/users",          usersController::getMany);
+        app.get   ("/users/{userId}", usersController::getOne);
+        app.delete("/users/{userId}", usersController::delete);
 
-    // Users routes
-    app.post("/users", usersController::create);
-    app.get("/users", usersController::getMany);
-    app.get("/users/{id}", usersController::getOne);
-    app.put("/users/{id}", usersController::update);
-    app.delete("/users/{id}", usersController::delete);
+        // Auth routes
+        app.post  ("/login",    authController::login);
+        app.post  ("/logout",   authController::logout);
+        app.get   ("/profile",  authController::profile);
 
-    app.start(PORT);
-  }
+        // Message Route
+        app.post  ("/messages",         messagesController::create);
+        app.patch ("/messages/{msgId}", messagesController::update);
+        app.get   ("/messages/mine",    messagesController::getMine);
+        app.get   ("/messages",         messagesController::getAll);
+        app.delete("/messages/{msgId}", messagesController::delete);
+
+        app.start(PORT);
+    }
 }
